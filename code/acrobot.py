828a4dee98a02f121541c1dbe227be8a427430de
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2020-08-20 15:52:51 +0100

    In progress commit on branch counter.

--- code/acrobot.py
+++ code/acrobot.py
@@ -7,14 +7,13 @@ def main(device, norm, rtol=1e-4, atol=1e-4):
     batch_size = 128
     name = 'acrobot/' + '-'.join(map(str, [norm, rtol, atol])).replace('.', '-')
     save = True
-    max_epochs = 100
+    max_epochs = 50
     lr = 1e-3
     weight_decay = 0.01
 
     times, train_dataloader, val_dataloader, test_dataloader = datasets.acrobot(batch_size)
 
     model = models.SymODE(times, norm, rtol, atol)
-    return model
 
     return train.main(name, train_dataloader, val_dataloader, test_dataloader, device, model, save, max_epochs, lr,
                       weight_decay)
